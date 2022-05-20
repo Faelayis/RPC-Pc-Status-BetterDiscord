a119088fc9c2c7b2a22322874a5081a92dbce525
Faelayis <48393914+Faelayis@users.noreply.github.com>
2022-05-20 23:04:57 +0700

    Update releases.js

--- script/releases.js
+++ script/releases.js
@@ -7,25 +7,21 @@ try {
 		version[0] = await data.split("* @author")[0].split("@version")[1].trim();
 		fs.readFile("RPCPcStatus.plugin.js", 'utf8', async (err, data) => {
 			version[1] = await data.split("* @author")[0].split("@version")[1].trim();
-			check();
+			if (version[0] === package.version) {
+				fs.readFile("README.md", 'utf8', async (err, data) => {
+					if (err) throw err;
+					data = data.replace(`/${version[1]}/`, `/${package.version}/`);
+					fs.writeFile("README.md", data, function (err) {
+						console.log("Update README.md");
+					});
+				});
+				fs.copyFile("RPCPcStatus.devlop.js", "RPCPcStatus.plugin.js", (err) => {
+					if (err) throw err;
+					console.log("Releases RPCPcStatus.plugin.js Done.");
+				});
+			}
 		})
 	})
 } catch (error) {
 	console.error(error);
-}
-
-function check() {
-	if (version[0] === package.version) {
-		fs.readFile("README.md", 'utf8', async (err, data) => {
-			if (err) throw err;
-			data = data.replace(`/${version[1]}/`, `/${package.version}/`);
-			fs.writeFile("README.md", data, function (err) {
-				console.log("Update README.md");
-			});
-		});
-		fs.copyFile("RPCPcStatus.devlop.js", "RPCPcStatus.plugin.js", (err) => {
-			if (err) throw err;
-			console.log("Releases RPCPcStatus.plugin.js Done.");
-		});
-	}
 }
