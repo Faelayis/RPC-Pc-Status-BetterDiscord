b44ac4bdd6fbe3c0474de883c3c47b35707bdce9
Faelayis <48393914+Faelayis@users.noreply.github.com>
2022-05-24 19:22:33 +0700

    Update script auto update version

--- script/releases.js
+++ script/releases.js
@@ -1,10 +1,19 @@
 const fs = require("fs");
-const package = require("../package.json");
-const package_dev = require("../plugins/devlop/package.json");
+const editJsonFile = require("edit-json-file");
+const package_dir = ["./../package.json", "./../plugins/devlop/package.json"];
+let version;
 
 try {
 	(async () => {
-		let version;
+		if (process.env.version_publish) {
+			for (const key of package_dir) {
+				await editJsonFile(`${__dirname}${key}`, {
+					autosave: true,
+				}).set(["version", "info.version"][package_dir.indexOf(key)], process.env.version_publish);
+			}
+		}
+		const package = require("../package.json");
+		const package_dev = require("../plugins/devlop/package.json");
 		fs.readFile("RPCPcStatus.plugin.js", "utf8", async (err, data) => {
 			version = await data.split("* @author")[0].split("@version")[1].trim();
 			if (package_dev.info.version === package.version) {
