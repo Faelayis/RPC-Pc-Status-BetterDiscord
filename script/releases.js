044df7927a679ef57371587d02b230acf314a2c4
Faelayis <48393914+Faelayis@users.noreply.github.com>
2022-05-28 10:00:47 +0700

    Fix bump package version devlop

--- script/releases.js
+++ script/releases.js
@@ -18,28 +18,38 @@ try {
 				core.notice(`npm check updates:\n ${JSON.stringify(x)}`);
 			});
 		if (process.env.bumpversion) {
-			const package = require("../package.json");
-			core.info(`${process.env.bumpversion} Bump package version: ${package.version}`);
+			const { version } = require("../package.json");
+			core.info(`${process.env.bumpversion} Bump package version: ${version}`);
 			for (const key of package_directory) {
 				await editJsonFile(`${__dirname}/${key}`, {
 					autosave: true,
-				}).set(["version", "info.version"][package_directory.indexOf(key)], package.version);
+				}).set(["version", "info.version"][package_directory.indexOf(key)], version);
 			}
-		}
-		fs.readFile("RPCPcStatus.plugin.js", "utf8", async (err, data) => {
-			const version = await data.match(semVer)[0],
-				package = require("../package.json");
-			if (require("../src/package.json").info.version === package.version) {
-				core.notice(`RPC Pc Status Upgrade ${version} -> ${package.version}`);
-				fs.readFile("README.md", "utf8", async (err, data) => {
-					data = await data.replace(semVer, package.version);
-					fs.writeFile("README.md", data, function (err) {
-						core.info("Update README.md");
-						if (err) throw err;
+			fs.readFile("RPCPcStatus.plugin.js", "utf8", async (err, data) => {
+				const version_old = await data.match(semVer)[0],
+					{ version } = require("../package.json");
+				if (require("../src/package.json").info.version === version) {
+					core.notice(`RPC Pc Status Upgrade ${version_old} -> ${version}`);
+					fs.readFile("README.md", "utf8", async (err, data) => {
+						data = await data.replace(semVer, version);
+						fs.writeFile("README.md", data, function (err) {
+							core.info("Update README.md");
+							if (err) throw err;
+						});
 					});
-				});
-			}
-		});
+				}
+			});
+		} else if (process.env.bumprelease) {
+			fs.readFile("RPCPcStatus.devlop.js", "utf8", async (err, data) => {
+				const version_old = await data.match(semVer)[0],
+					{ version } = require("../packagedev.json");
+				core.notice(`RPC Pc Status devlop pre release ${version_old} -> ${version}`);
+				core.info(`${process.env.bumpversion} Bump package version: ${version}`);
+				await editJsonFile(`${__dirname}/./../src/package.json`, {
+					autosave: true,
+				}).set("info.version", version);
+			});
+		}
 	})();
 } catch (error) {
 	core.setFailed(`Action failed with error ${error}`);
